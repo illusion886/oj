19d147ea806b008aaf46ec5bbeaba656992d11d3
徐威 <2834553532@qq.com>
2022-07-05 00:29:45 +0800

    121

--- 平方数.cpp
+++ 平方数.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 #define ll long long;
 #define inf 0x3f3f3f3f;
-int a[35];
+int a[36];
 int dp[1001][1001];
 int main() {
 	int n; cin >> n;
-	for (int i = 1; i <= 34; i++) {
+	for (int i = 1; i <= 35; i++) {
 		a[i] = i * i;
 	}
 	dp[0][0] = 1;
-	for (int i = 1; i <= 34; i++) {
+	for (int i = 1; i <= 35; i++) {
 		for (int j = 0; j <= n; j++) {
 			for (int k = 0; k * a[i] <= j; k++) {
-				dp[i][j] = dp[i - 1][j-k*a[i]];
+				dp[i][j] += dp[i - 1][j-k*a[i]];
 			}
 		}
 	}
-	cout << dp[34][n];
+	cout << dp[35][n];
 }
