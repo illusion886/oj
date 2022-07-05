c71bc21ef00cb028d75d257526566d662915f17e
徐威 <2834553532@qq.com>
2022-07-05 16:53:29 +0800

    spfa垃圾把

--- SPFA.cpp
+++ SPFA.cpp
@@ -1,11 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
+#include<string.h>
+#include<cstdio>
 using namespace std;
 const int maxn = 1e5 + 10;
 typedef struct node {
 	int x, y;
-	node(int x=0,int y=0):x(x),y(y) {
+	node(int x = 0, int y = 0) :x(x), y(y) {
 	};
 };
 vector <node> a[maxn];
@@ -19,11 +23,11 @@ void spfa(int s) {
 	q.push(s);
 	while (!q.empty()) {
 		int k = q.front(); q.pop();
-		v[k] = false;
-		for (int i = 0; i <a[k].size(); i++) {
+		v[k] = true;
+		for (int i = 0; i < a[k].size(); i++) {
 			int x = a[k][i].x;
 			int y = a[k][i].y;
-			if (d[k]+y< d[x]) {
+			if (d[k] + y < d[x]) {
 				d[x] = d[k] + y;
 				if (v[x]) {
 					q.push(x);
@@ -37,12 +41,12 @@ int main() {
 	cin >> n >> m >> s;
 	for (int i = 1; i <= m; i++) {
 		int q, w, e;
-		cin >> q >> w>>e;
+		scanf("%d%d%d", &q, &w, &e);
 		a[q].push_back(node(w, e));
 	}
 	spfa(s);
-	for (int i = 1; i <= m; i++) {
-		if (d[1] = d[0]) cout << (1 << 31) - 1 << " ";
+	for (int i = 1; i <= n; i++) {
+		if (d[i] == d[0]) cout << (1 << 31) - 1 << " ";
 		else cout << d[i] << " ";
 	}
 	return 0;
